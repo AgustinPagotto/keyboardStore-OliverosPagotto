425c30a3f6e940a88f0c038fc47c0ccf2c4f81be
agustinpagotto <agustinpagotto@gmail.com>
2022-08-16 16:20:43 -0300

    basic component creation

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header className="main-container">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.google.com">Productos</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Nosotros</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Contactos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
